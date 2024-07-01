2d9cb596627341378ac8095739e2e0250412f779
ddianjn <ddian.cz@gmail.com>
2024-07-01 15:19:17 -0400

    Create position.py

---
+++ backtest/position.py
@@ -0,0 +1,34 @@
+from colorama import Fore, Style
+
+class Position:
+  def __init__(self,
+               stock: str, date,
+               price: float64,
+               shares: float32):
+    self.stock = stock
+    self.open_date = date
+    self.open_price = price
+    self.shares = shares
+    self.close_date = None
+    self.close_price = None
+    self.profit = None
+
+  def close(self, date, price: float64):
+    self.close_date = date
+    self.close_price = price
+    self.profit = (self.close_price - self.open_price) * self.shares
+
+  def is_closed(self):
+    return self.close_date is not None
+
+  def __eq__(self, other: Position):
+    return self.stock == other.stock and self.open_date == other.open_date and self.open_price == other.open_price
+
+  def print(self):
+    if self.is_closed():
+      percent_profit = (self.close_price - self.open_price) / self.open_price
+      color = Fore.GREEN if self.profit > 0 else Fore.RED
+      print(f"{color}{stock} Open Date: {self.open_date}, Price: {self.open_price: .2f}, Shares: {self.shares: .2f}, Close Date: {self.close_date}, Close Price: {self.close_price}, Profit: {self.profit}, % Profit: {percent_profit}\n{Style.RESET_ALL}")
+    else:
+      color = Fore.GRAY
+      print(f"{color}{stock} Open Date: {self.open_date}, Price: {self.open_price: .2f}, Shares: {self.shares: .2f}\n{Style.RESET_ALL}")
