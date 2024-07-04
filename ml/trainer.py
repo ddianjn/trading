dc927f3b8520bd156ca4e2fbf300465e501f87a3
ddianjn <ddian.cz@gmail.com>
2024-07-04 18:00:20 -0400

    Update trainer.py

--- ml/trainer.py
+++ ml/trainer.py
@@ -13,6 +13,16 @@ class ModelTrainer:
                early_stop_patience = 30,
                verbose = False,
                print_train_steps = True) -> (float64, float64):
+    """Train and evaluate the model.
+
+    Returns:
+      Train loss and validation loss.
+    """
 
   def eval(self, x, y, shuffle: bool = False) -> (float64, np.ndarray):
+    """Evaluate the model.
+
+    Returns:
+      Loss and the prediction result.
+    """
     
