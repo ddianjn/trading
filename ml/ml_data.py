b38cd9bb237fde263ee98bc9405b6a7256096a02
ddianjn <ddian.cz@gmail.com>
2024-07-11 11:45:09 -0400

    Update ml_data.py

--- ml/ml_data.py
+++ ml/ml_data.py
@@ -8,6 +8,7 @@ from typing import List, Dict
 
 class TrainingData:
   def __init__(self,
+               raw_data: pd.DataFrame,
                train_data: pd.DataFrame,
                validate_data: pd.DataFrame,
                test_data: pd.DataFrame,
@@ -20,6 +21,7 @@ class TrainingData:
                train_indices: List,
                validate_indices: List,
                test_indices: List):
+    self.raw_data = raw_data
     self.train_data = train_data
     self.validate_data = validate_data
     self.test_data = test_data
@@ -33,11 +35,18 @@ class TrainingData:
     self.validate_indices = validate_indices
     self.test_indices = test_indices
 
+class TrainingDatas:
+  def __init__(self, data_dict: Dict[TrainingData]):
+    self.data_dict = data_dict
+    self.train_x = np.vstack([d.train_x for _, d in data_dict])
+    self.train_y = np.vstack([d.train_y for _, d in data_dict])
+    self.validate_x = np.vstack([d.validate_x for _, d in data_dict])
+    self.validate_y = np.vstack([d.validate_y for _, d in data_dict])
 
-def prepare_sequence_data(stock_ticker:str,
-                 start:str = "2018-01-01",
-                 end:str = None,
-                 interval:str = "1d",
+def prepare_sequence_data(stock_tickers: str|List[str],
+                 start: str = "2018-01-01",
+                 end: str = None,
+                 interval: str = "1d",
                  scalers: Dict|None = None,
                  features: List[str] = ["Close", "High", "Low", "Volume"],
                  output_features: List[str] = ['Close'],
@@ -51,24 +60,19 @@ def prepare_sequence_data(stock_ticker:str,
                  print_raw_data: bool = False,
                  print_scaled_data: bool = False,
                  print_time_series_data: bool = False):
-  stock_data = data_fetching.download_data(stock_ticker, start, end, interval = interval)
-  if plot_candle_chart:
-    plotting.plot_candlestick(stock_data)
+  stock_datas, train_datas, validate_datas, test_datas = _fetch_raw_datas(stock_tickers = stock_tickers,
+                                                                          start = start,
+                                                                          end = end,
+                                                                          interval = interval,
+                                                                          feature_generators = feature_generators,
+                                                                          validate_size = validate_size,
+                                                                          test_size = test_size,
+                                                                          plot_candle_chart = plot_candle_chart,
+                                                                          plot_split_chart = plot_split_chart,
+                                                                          print_raw_data = print_raw_data)
+  scalers = _create_scalers(train_datas)
 
-  for generator in feature_generators:
-    stock_data = generator(stock_data)
-
-  train_data, validate_data, test_data = data_processing.split_data(stock_data,
-                                                    validate_size = validate_size,
-                                                    test_size = test_size,
-                                                    plot_split_chart = plot_split_chart)
-
-  if print_raw_data:
-    print(f"\nFeatures\n{train_data.columns}")
-    print(f"train_data\n{train_data.tail()}")
-    print(f"validate_data\n{validate_data.tail()}")
-    print(f"test_data\n{test_data.tail()}")
-
+  merged_train_data = pd.concat(train_datas.values())
   # Normalize
   scaled_train_data, scaled_validate_data, scaled_test_data, scalers = data_processing.normalize_data(train_data, validate_data, test_data, scalers = scalers)
   if print_scaled_data:
@@ -243,3 +247,51 @@ def _create_feature_label_data(data: pd.DataFrame,
     print(f"==data_y==\n{data_y[:3]}\n")
     print(f"==data_y size: {len(data_y)}==")
   return data_x, data_y, data_indices
+
+def _fetch_raw_datas(stock_tickers: str|List[str],
+                 start: str = "2018-01-01",
+                 end: str = None,
+                 interval: str = "1d",
+                 feature_generators: List[Callable[pd.DataFrame, pd.DataFrame]] = [],
+                 validate_size: int = 30,
+                 test_size: int = 30,
+                 plot_candle_chart: bool = False,
+                 plot_split_chart: bool = False,
+                 print_raw_data: bool = False):
+  if stock_tickers is str:
+    stock_tickers = [stock_tickers]
+
+  stock_datas = {}
+  train_datas = {}
+  validate_datas = {}
+  test_datas = {}
+  for stock_ticker in stock_tickers:
+    stock_data = data_fetching.download_data(stock_ticker, start, end, interval = interval)
+    if plot_candle_chart:
+      plotting.plot_candlestick(stock_data)
+
+    for generator in feature_generators:
+      stock_data = generator(stock_data)
+    stock_datas[stock_ticker] = stock_data
+
+    train_data, validate_data, test_data = data_processing.split_data(stock_data,
+                                                      validate_size = validate_size,
+                                                      test_size = test_size,
+                                                      plot_split_chart = plot_split_chart)
+
+    if print_raw_data:
+      print(f"\nFeatures\n{train_data.columns}")
+      print(f"train_data\n{train_data.tail()}")
+      print(f"validate_data\n{validate_data.tail()}")
+      print(f"test_data\n{test_data.tail()}")
+    train_datas[stock_ticker] = train_data
+    validate_datas[stock_ticker] = validate_data
+    test_datas[stock_ticker] = test_data
+  return stock_datas, train_datas, validate_datas, test_datas
+
+def _create_scalers(train_datas: Dict[str, pd.DataFrame]):
+  merged_train_data = pd.concat(train_datas.values())
+  _, _, _, scalers = data_processing.normalize_data(merged_train_data,
+                                                    merged_train_data[0],
+                                                    merged_train_data[0])
+  return scalers
