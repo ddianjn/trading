e0b83d612d688d1f046c8e611f99a8c333ed2873
ddianjn <ddian.cz@gmail.com>
2024-07-12 16:31:54 -0400

    Update indicators.py

--- indicators.py
+++ indicators.py
@@ -2,4 +2,4 @@ import numpy as np
 import pandas as pd
 
 def sma(data, period: int):
-  return np.array(data['Close'].rolling(window=period).mean())
+  return data['Close'].rolling(window=period).mean()
