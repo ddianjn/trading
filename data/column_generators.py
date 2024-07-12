57e0827cfe91ba0ea16bcae2e8c925e7dfd3b8dd
ddianjn <ddian.cz@gmail.com>
2024-07-12 13:41:40 -0400

    Update column_generators.py

--- data/column_generators.py
+++ data/column_generators.py
@@ -9,8 +9,11 @@ def generate_return(data: pd.DataFrame) -> None:
 def lag_features(lag = 1):
   def lag_feature_gnerator(data: pd.DataFrame):
     columns = data.columns
+    new_columns = {}
     for i in range(1, lag + 1):
       for column in columns:
-        data[f'lag_{i}_{column}'] = data[column].shift(i)
+        new_columns[f'lag_{i}_{column}'] = data[column].shift(i)
+    print(pd.DataFrame(new_columns))
+    data = pd.concat([data, pd.DataFrame(new_columns)], axis=1)
     return data.dropna()
   return lag_feature_gnerator
