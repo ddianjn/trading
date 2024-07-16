d2789623570b1c8f385a3ff2c14f8d4b4466c6b0
ddianjn <ddian.cz@gmail.com>
2024-07-16 17:25:28 -0400

    Update data_fetching.py

--- data/data_fetching.py
+++ data/data_fetching.py
@@ -9,16 +9,17 @@ def download_data(stock_ticker: str,
   if end is None:
     end = date.today().strftime("%Y-%m-%d")
   # Load the data from Yahoo Finance
-  historical_data = yf.download(stock_ticker, start=start, end=end, interval=interval)
+  # historical_data = yf.download(stock_ticker, start=start, end=end, interval=interval)
+  stock = yf.Ticker(stock_ticker)
+  historical_data = stock.history(start=start, end=end, interval=interval)
   print()
-  # historical_data.reset_index(inplace=True)
 
+  # historical_data.reset_index(inplace=True)
   # Parse dates and convert them to matplotlib's date format
   # historical_data['Date'] = pd.to_datetime(historical_data['Date'])
   # historical_data['Date'] = historical_data['Date'].apply(mdates.date2num)
 
   # Other yf functions
-  # stock = yf.Ticker(stock_ticker)
   # historical_data = stock.history(period=period)
   # stock.info
   # stock.cashflow
